d8af6d5daecfd1c1f1c19b31dd4f726ec558b217
laihuamin <1445509994@qq.com>
2017-11-26 00:07:21 +0800

    按钮联动完成

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import Btn from '../btn/btn.js';
+import './app.less';
 
 class App extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            nowLocal: 0
+        }
     };
-    turn () {
+    turn (n) {
+        let _n = this.state.nowLocal + n;
+        this.setState({nowLocal: _n});
     };
     render () {
         return (
             <div className="app">
-                <Btn turn={this.turn} count={10} nowLocal={1}></Btn>
+                <Btn turn={this.turn.bind(this)} count={10} nowLocal={this.state.nowLocal}></Btn>
             </div>
         )
     };
