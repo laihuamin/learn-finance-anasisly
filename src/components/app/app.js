23497ed07deb4f4c5a5420dc7ee63373fe5b7545
laihuamin <1445509994@qq.com>
2017-11-27 22:34:46 +0800

    app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import Btn from '../btn/btn.js';
 import './app.less';
 import ImgGroup from '../img-group/imgGroup.js';
 
+
 class App extends React.Component{
     constructor(props) {
         super(props);
@@ -15,36 +16,59 @@ class App extends React.Component{
         }
     };
     formatData() {
-        const left = [],
-            top = [],
-            rorate = [],
-            data = [
-
-            ];
-        left = this.randomLeft();
-        top = this.randomTop();
-        rorate = this.randomRorate();
+        const left = this.randomLeft(),
+            top = this.randomTop(),
+            rorate = this.randomRorate(),
+            data = [];
+        for(let i = 0; i < 10; i++) {
+            data[i] = Object.assign({}, {
+                img: 'http://img.htmleaf.com/1507/3d-club-images-2.jpg',
+                title: '小勺子'
+            });
+        }
         this.setState({
-            left,
-            top,
-            rorate,
-            data
+            left: left,
+            top: top,
+            rorate: rorate,
+            data: data
         })
     };
     randomLeft() {
         const left = [];
-        for(let i = 0; i < this.state.data.length; i++){
-            left[i] = Math.random()
+        for(let i = 0; i < 5; i++){
+            left[i] = Math.floor(Math.random() * 20);
         }
+        for(let i = 5; i < 10; i++){
+            left[i] = Math.floor(Math.random() * 20 + 80);
+        }
+        return left;
     };
+    randomTop() {
+        const top = [];
+        for(let i = 0; i < 10; i++){
+            top[i] = Math.floor(Math.random() * 80);
+        }
+        return top;
+    };
+    randomRorate() {
+        const rorate = [];
+        for(let i = 0; i < 5; i++){
+            rorate[i] = Math.floor(Math.random() * 45);
+        }
+        for(let i = 5; i < 10; i++){
+            rorate[i] = -Math.floor(Math.random() * 45);
+        }
+        return rorate;
+    }
     turn (n) {
         let _n = this.state.nowLocal + n;
         this.setState({nowLocal: _n});
     };
-    componentDidMount(){
+    componentWillMount(){
         this.formatData();
     }
     render () {
+        console.log(this.state);        
         return (
             <div className="app">
                 <ImgGroup turn={this.turn.bind(this)} count={10} nowLocal={this.state.nowLocal} left={this.state.left} top={this.state.top} data={this.state.data} rorate={this.state.rorate}/>
