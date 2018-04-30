5508e124e7541c605c8897f759a09d158e322b32
laihuamin <1445509994@qq.com>
2018-04-30 15:24:52 +0800

    删除venv

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/app.js';
-
-ReactDOM.render(
-    <App></App>,
-    document.getElementById('root')
-)
